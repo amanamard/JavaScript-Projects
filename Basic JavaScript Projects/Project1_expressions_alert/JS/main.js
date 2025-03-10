d6f7b2a111b114009d2bb2ac989d4b0f877a4a5e
amanamard <104510239+amanamard@users.noreply.github.com>
2025-03-09 23:50:43 -0400

    Added Project1_expressions_alert folder
    This is my basic JavaScript assignment.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var Hello = "Hello, there!"; // Defining variable Hello and assigning it a value
+window.alert(Hello); //Popping up an alert window with the value of Hello
+
+document.write("ASSIGNMENT<br>"); // Displaying text
+
+var Sent1 = "This is the beginning of the string"; // Definning variable Sent1 and assigning it a value
+var Sent2 = " and this is the end of the string"; // Defining variable Sent1 and assigning it a value
+var Sent = Sent1 + Sent2; // Defining variable Sent and assigning it the value of an expression
+document.write(Sent); // Displaying the value of variable Sent
+
+
+
+
+
